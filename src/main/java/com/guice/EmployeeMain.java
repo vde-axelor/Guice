af33c76966b65e06184843b8c2408ac796014af5
vde-axelor <v.deasi@axelor.com>
2023-01-17 16:30:51 +0530

    demo example of guice

---
+++ src/main/java/com/guice/EmployeeMain.java
@@ -0,0 +1,14 @@
+package com.guice;
+
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+//		Resource r=new ClassPathResource("applicationContext.xml");  
+//        BeanFactory factory=new XmlBeanFactory(r);  
+          
+//        Employee s=(Employee)factory.getBean("e");  
+//        s.show();  
+	}
+
+}
